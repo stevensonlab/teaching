5fa3380b7ec051866041893608609b0cbc92efd6
Ian Stevenson <ihstevenson@users.noreply.github.com>
2023-02-02 12:41:11 -0500

    improvements to lab2

--- sand/labs/lab2/lab2_solution.m
+++ sand/labs/lab2/lab2_solution.m
@@ -6,46 +6,48 @@
 data = readtable('Hursh1936.csv');
 
 % or log-Body Weight vs log-Brain Weight
-data = readtable('BodyBrain.csv');
+% data = readtable('BodyBrain.csv');
 
 data = table2array(data);
+x = data(:,1);
+y = data(:,2);
+n = size(data,1);
 
 %% 2.1 Bootstrap to estimate the mean and sem of the x-variable
 
 nboot = 10000;
-bootstat = bootstrp(nboot,'mean',data(:,1));
+bootstat = bootstrp(nboot,'mean',x);
 figure(1)
 subplot(2,1,1)
-histogram(data(:,1),50,'EdgeColor','none')
-xlim([min(data(:,1)) max(data(:,1))])
+histogram(x,50,'EdgeColor','none')
+xlim([min(x) max(x)])
 subplot(2,1,2)
 histogram(bootstat,50,'EdgeColor','none')
-xlim([min(data(:,1)) max(data(:,1))])
+xlim([min(x) max(x)])
 
 %% 2.2 Plot the data
 
 figure(2)
-plot(data(:,1),data(:,2),'o')
+plot(x,y,'o')
 
 % 2.3 Linear regression
 
 % linear regression
-b = polyfit(data(:,1),data(:,2),1);
+b = polyfit(x,y,1);
 
 % 2.4 Plot the data and fit
-yhat = polyval(b,data(:,1));
+yhat = polyval(b,x);
 
 hold on
-plot(data(:,1),yhat)
+plot(x,yhat)
 hold off
 
 %% 2.5 Bootstrap to find error-bars for the model parameters
 
 nboot = 1000;
-[bootstat,bootsam] = bootstrp(nboot,'',1:size(data,1));
 for i=1:nboot
-    idx = bootsam(:,i);
-    bboot(i,:) = polyfit(data(idx,1),data(idx,2),1);
+    sample_indices = randsample(n,n,true);
+    bboot(i,:) = polyfit(x(sample_indices),y(sample_indices),1);
 end
 
 figure(3)
@@ -54,12 +56,14 @@ histogram(bboot(:,1),50)
 subplot(1,2,2)
 histogram(bboot(:,2),50)
 
+% can also be done in one line...
+% bootstat = bootstrp(nboot,'polyfit',x,y,1);
+
 %% Compare different ways of doing linear regression
 
-b = polyfit(data(:,1),data(:,2),1);
+b = polyfit(x,y,1);
 
-X = [data(:,1)*0+1 data(:,1)];
-y = data(:,2);
+X = [x*0+1 x];
 
 b1 = X\y;
 b2 = pinv(X)*y;
