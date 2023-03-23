c26413c04cd55ee511d58d5e5d00d11d395a9e34
Ian Stevenson <ihstevenson@users.noreply.github.com>
2023-03-23 12:32:06 -0400

    Update lab6_solution.m

--- sand/labs/lab6/lab6_solution.m
+++ sand/labs/lab6/lab6_solution.m
@@ -1,39 +1,53 @@
+
+% data{dataset_number}.spikes is [neurons x stimulus_conditions x time x repeated_trials]
+% data{dataset_number}.conditions contains information about the trial orientation/contrast
+
 dataset_number = 8;
 
-
-cond1 = 2; % 0deg at 100% contrast
-cond2 = 10; % 90deg at 100% contrast
+cond1 = 2;  % orientation=0  contrast=100
+cond2 = 10; % orientation=90 contrast=100
 X1 = squeeze(data{dataset_number}.spikes(:,cond1,:,:));
 X2 = squeeze(data{dataset_number}.spikes(:,cond2,:,:));
 
-X1 = squeeze(sum(X1,2));
-X2 = squeeze(sum(X2,2));
-figure(2)
-neuron=5;
-plot([X1(neuron,:) X2(neuron,:)],rand(1,size(X1,2)*2)/10+[X1(neuron,:)*0 X2(neuron,:)*0+1],'o')
+% total numbers of spikes during the trial
+total_X1 = squeeze(sum(X1,2));
+total_X2 = squeeze(sum(X2,2));
 
-%%
+%% Lab 6.1 - Visualize predictors
+% Our goal is to take the activity of all neurons (summed over some time
+% range) and discriminate the trials for two specific conditions. We will
+% need a [trials x neurons] matrix X that contains the neural activity as
+% well as a [trials] vector y that contains the condition identity (0 or 1).
+
+% Concatenate predictor matrices
+X =[total_X1 total_X2]';
+% Concatenate response vector
+y1 = ones(size(total_X1,2),1);
+y2 = zeros(size(total_X2,2),1);
+y  = [y1; y2];
+
 figure(1)
-subplot(1,2,1)
-imagesc([X1 X2]');
-line(xlim(),[1 1]*size(X1,2),'Color','r')
+imagesc(X);
+line(xlim(),[1 1]*size(total_X1,2),'Color','r')
 xlabel('Neurons')
 ylabel('Trials')
 
-%%
-X = [X1 X2]';
-% X = X(:,1:5);
-% Concatenate response vector
-y1 = ones(size(X1,2),1);
-y2 = zeros(size(X2,2),1);
-y  = [y1; y2];
+% see neuron 5 vs neuron 6
+figure(2)
+gscatter(X(:,5),X(:,6),y)
+xlabel('Neuron 5')
+ylabel('Neuron 6')
 
-% Fit all of the data...
+%% Lab 6.2 - Logistic regression
+
 b = glmfit(X,y,'binomial'); % Fit
+
+%% Lab 6.3 -- Calculate accuracy
+
 yhat = glmval(b,X,'logit'); % Predict
 sum((yhat>0.5)==y)/length(y) % Accuracy
-%%
-% Cross-validation
+
+%% BONUS Cross-validation
 cross_validation = cvpartition(size(X,1),'KFold',10);
 yhat=y*NaN;
 for cvk = 1:cross_validation.NumTestSets
@@ -42,8 +56,12 @@ for cvk = 1:cross_validation.NumTestSets
 end
 sum((yhat>0.5)==y)/length(y) % Accuracy
 
-%% compare over time
+%% BONUS LDA
+class = classify(X,X,y,'linear');
+sum(class==y)/length(y) % Accuracy
 
+
+%% BONUS accuracy over time
 
 cond1 = 2;
 cond2 = 10;
@@ -51,13 +69,7 @@ cond2 = 10;
 X1 = squeeze(data{dataset_number}.spikes(:,cond1,:,:));
 X2 = squeeze(data{dataset_number}.spikes(:,cond2,:,:));
 
-figure(1)
-subplot(1,2,1)
-imagesc([squeeze(sum(X1,2))'; squeeze(sum(X2,2))']);
-line(xlim(),[1 1]*size(X1,3),'Color','r')
-xlabel('Neurons')
-ylabel('Trials')
-subplot(1,2,2)
+figure(3)
 
 % Two conditions to compare
 cond1 = 1;
