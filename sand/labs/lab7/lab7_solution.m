2dcc2221a7b7020b875120f374090285775d6b02
Ian Stevenson <ihstevenson@users.noreply.github.com>
2023-03-29 11:04:37 -0400

    update lab7

--- sand/labs/lab7/lab7_solution.m
+++ sand/labs/lab7/lab7_solution.m
@@ -2,21 +2,21 @@
 %% Waveforms from 8-electrodes...
 
 figure(1)
-% plot(???)
+plot(X(:,1:100:end))
 xlabel('Time x ElectrodeNumber'); ylabel('Voltage')
 
 %% Principal Components Analysis
 
-% [???]=pca(???);
+[C,S,vexp]=pca(X');
 
 % Plot first two PC scores
 figure(2)
-% plot(???)
+plot(S(:,1),S(:,2),'.')
 xlabel('PC1'); ylabel('PC2')
 axis equal
 
 % Reconstruction from first k PCs
-% k=???;
+k=25;
 m = mean(X');
 Xhat = bsxfun(@plus,S(:,1:k)*C(:,1:k)',m)';
 
@@ -28,23 +28,29 @@ legend({'Original','Reconstruction'})
 
 % Variance Explained
 figure(4)
-% plot(???)
+plot(cumsum(vexp)/sum(vexp))
 xlabel('PCs'); ylabel('Fraction of Variance Explained')
+
+%% bonus plot
+figure(10)
+plot(1:256,C(:,1),1:256,C(:,2))
 
 %% K-Means Clustering...
 
-% k = ??;
-% [???] = kmeans(???);
+rng(1)
+k = 20;
+[idx,c,sumd] = kmeans(S(:,1:25),k);
 
 figure(5)
 cmap = lines(k);
 for i=1:k
-%     plot(???,'.','Color',cmap(i,:));
+    plot(S(idx==i,1),S(idx==i,2),'.','Color',cmap(i,:));
     hold on
 end
 hold off
 drawnow
 xlabel('PC1'); ylabel('PC2')
+box off; set(gca,'TickDir','out'); axis equal
 
 %% Show Clusters from the Buzsaki Lab for Comparison...
 
@@ -53,7 +59,7 @@ idx = Clu;
 k = max(Clu);
 cmap = lines(k);
 for i=1:k
-%     plot(???,'.','Color',cmap(i,:));
+    plot(S(idx==i,1),S(idx==i,2),'.','Color',cmap(i,:));
     hold on
 end
 hold off
@@ -73,3 +79,14 @@ for i=1:k
     axis tight
     ylabel(['Cluster ' num2str(i)])
 end
+
+%% Bonus - evaluate sum squared distances for k-means with varying k
+ssdk=[];
+for k=1:20
+    for rep=1:5
+        [~,~,sumd] = kmeans(S(:,1:5),k);
+        ssdk(k,rep)=sum(sumd);
+    end
+end
+figure(8)
+plot(ssdk)
