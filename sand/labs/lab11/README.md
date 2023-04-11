2f61c129a44f5aa638dd0fc8e7f175bdf5623166
Ian Stevenson <ihstevenson@users.noreply.github.com>
2023-04-11 11:12:48 -0400

    add lab11

---
+++ sand/labs/lab11/README.md
@@ -0,0 +1,32 @@
+# Lab 11
+
+This lab is designed to introduce you to some of the basic processing involved in analyzing Calcium imaging. The goal is to complete a script with some basic visualization, motion artifact correct, ROI extraction, and deconvolution.
+
+Start by downloading the code (.m file) and data. This data contains a short segment of a two-photon recording from mouse primary visual cortex using GCaMP6s taken from [CRCNS pvc-7](https://crcns.org/data-sets/vc/pvc-7/about-pvc-7).
+
+## Goal
+
+![alt text](https://github.com/stevensonlab/teaching/raw/master/sand/labs/lab11/assets/output.png)
+
+![alt text](https://github.com/stevensonlab/teaching/raw/master/sand/labs/lab11/assets/output2.png)
+
+## Part 1: Visualization
+
+Use `imagesc` to show a frame of the movie alongside the average over all time. You can experiment with different colormaps using `colormap gray` or `colormap turbo`.
+
+Since this is spatiotemporal (e.g. video) data it is often helpful to visualize as a movie. Complete the `for` loop to geneterate this. Note that there are some noticeable motion artifacts.
+
+## Part 2: Estimating and correcting for motion artifacts
+
+There are many methods for correcting for motion artifacts. The first section in Part 2 shows a basic comparison of two sequential frames in the movie.
+
+To track the difference between the frames one strategy is to use the 2D cross-correlation (`xcorr2`). There are also some basic registration methods built into matlab's image processing toolbox. `imregtform` estimates a transformation between the successive frames. Try completing the code for `imwarp` that uses the ouput of `imregtform` to "register" `frame2` with `frame1`.
+
+## Part 3: ROI extraction
+
+Regions of interest (ROIs) can also be identified using several different techniques. When cells overlap it can be beneficial to excliticly separate parts of the video into spatial (ROIs) and temporal fluorescence patterns. After running `pca`, try visualizing these patterns. Note that the spatial patterns are represented by the PCs themselves `C` and the temporal patterns are represented by the scores `S`.
+
+
+## Part 4: Deconvolution
+
+In some cases, researchers aim to remove the slow effects due to the timecourse of fluorescence and estimate underlying neural activity. Download the code for `fast_oopsi.m` (slightly modified from [Josh Vogelstein](https://github.com/jovo/oopsi)). Trying running this section as is. Then try modifying the time constant for the fluorescence `tau`, and explore how things change for different ROIs.
---
+++ sand/labs/lab11/lab11_solution.m
@@ -0,0 +1,163 @@
+%% Lab 11: Calcium imaging
+
+load crcns_pvc7_small
+
+% data contains two-photon imaging data from primary visual cortex
+% https://crcns.org/data-sets/vc/pvc-7/about-pvc-7
+% and is [pixels x pixels x frames]
+
+%% Part 1: Visualizing image/video data
+
+% show first frame and average over time
+figure(1)
+subplot(1,2,1)
+imagesc(data(:,:,1))
+axis equal off% force square pixels
+line(xlim(),[218 218])
+title('First Frame')
+subplot(1,2,2)
+imagesc(mean(data,3))
+axis equal off % force square pixels
+line(xlim(),[218 218])
+title('Average')
+
+% show pattern over time for a single row of pixels
+figure(2)
+imagesc(squeeze(data(218,:,:)))
+xlabel('Time')
+ylabel('Pixels')
+
+%% Generate a video of the data
+figure(3)
+cl = [min(data(:)) max(data(:))];
+imagesc(data(:,:,1))
+colormap gray; axis equal off
+for i=1:size(data,3)
+    imagesc(data(:,:,i))
+    set(gca,'CLim',cl); % note: imagesc changes the colorscale
+    drawnow
+end
+
+%% Part 2: Estimating and correcting for motion artifacts
+
+time1 = 1753;
+time2 = 1754;
+
+% raw data
+frame1 = double(data(:,:,time1));
+frame2 = double(data(:,:,time2));
+
+% filter so that effect are easier to see
+h = fspecial('gaussian',20,5);
+frame1 = imfilter(frame1,h);
+frame2 = imfilter(frame2,h);
+
+% zoom window
+box.x=double([85 115]);
+box.y=double([210 240]);
+
+figure(4)
+subplot(2,4,1)
+imagesc(frame1)
+axis equal off
+rectangle('Position',[box.x(1) box.y(1) box.x(2)-box.x(1) box.y(2)-box.y(1)])
+subplot(2,4,2)
+imagesc(frame2)
+axis equal off
+rectangle('Position',[box.x(1) box.y(1) box.x(2)-box.x(1) box.y(2)-box.y(1)])
+subplot(2,4,3)
+imagesc(frame1-frame2)
+axis equal off
+rectangle('Position',[box.x(1) box.y(1) box.x(2)-box.x(1) box.y(2)-box.y(1)])
+
+subplot(2,4,5)
+imagesc(frame1)
+axis equal off
+xlim(box.x); ylim(box.y)
+subplot(2,4,6)
+imagesc(frame2)
+axis equal off
+xlim(box.x); ylim(box.y)
+subplot(2,4,7)
+imagesc(frame1-frame2)
+axis equal off
+xlim(box.x); ylim(box.y)
+cl = [min(frame1(:)-frame2(:)) max(frame1(:)-frame2(:))];
+
+%% Alignment
+
+% Option 1: find 2D cross-correlation
+R = xcorr2(frame1-mean(frame1(:)),frame2-mean(frame2(:)));
+[~,i]=max(R,[],'all');
+[ypeak,xpeak] = ind2sub(size(R),i);
+corr_offset = [(ypeak-size(frame2,1)) (xpeak-size(frame2,2))];
+
+% Option 2: using matlab image processing toolbox
+[optimizer, metric] = imregconfig('monomodal');
+tform = imregtform(frame2, frame1, 'affine', optimizer, metric);
+frame2_registered = imwarp(frame2,tform,'OutputView',imref2d(size(frame1)));
+
+subplot(2,4,4)
+imagesc(frame1-frame2_registered)
+axis equal off
+set(gca,'CLim',cl)
+subplot(2,4,8)
+imagesc(frame1-frame2_registered)
+axis equal off
+xlim(box.x); ylim(box.y)
+set(gca,'CLim',cl)
+
+%% Part 3: ROI extraction
+
+data_subset = data(131:190,131:190,:); % good
+X = double(reshape(data_subset,[],2000)');
+X = X./mean(X,2);
+[C,S]=pca(X);
+
+%% Visualize the principal components and their timeseries
+figure(5)
+for i=1:4
+    subplot(4,3,(i-1)*3+1)
+    imagesc(reshape(C(:,i),60,60))
+    axis equal
+    subplot(4,3,(i-1)*3+[2 3])
+    plot(S(:,i))
+end
+
+figure(6)
+scatter(S(:,1),S(:,2),5,1:size(X,1),'filled')
+xlabel('PC1'); ylabel('PC2'); axis equal
+
+%% Part 5: Deconvolution
+% what happens when you change tau?
+% note: this is GCaMP6s (https://blog.addgene.org/aav-encoded-calcium-sensors)
+% try a different ROI (it may help to go to figure(1) and set "axis on")
+
+% pick ROI center
+locx=160;
+locy=147;
+
+% preprocess DF/F
+y = squeeze(mean(mean(data((locx-5):(locx+5),(locy-5):(locy+5),:)),2));
+y = y./squeeze(mean(mean(data),2));
+% y = detrend(y);
+y = (y-min(y))/(max(y)-min(y))+eps;
+
+% initialize params
+tau   = 10; % time constant for decay
+V.dt    = 1/31;  % time step size
+P.gam   = 1-V.dt/tau; % C(t) = gam*C(t-1)
+P.a   = 1;
+P.b   = 0;
+V.fast_iter_max = 10;
+[n_best P_best V C]=fast_oopsi(y,V,P);
+
+% visualize
+t = [1:length(y)]'/31;
+
+figure(7)
+plot(t,y);
+hold on
+plot(t,C);
+plot(t,n_best)
+hold off
